3a9e8db6dbc3fe16a1a5ed49535ba63ba3e960ed
remackgeek <bmack@bluewiz.net>
2019-08-17 19:57:21 -0700

    fix hello component test

--- src/app/hello/hello.component.spec.ts
+++ src/app/hello/hello.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloComponent } from './hello.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 describe('HelloComponent', () => {
   let component: HelloComponent;
@@ -8,7 +10,11 @@ describe('HelloComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HelloComponent ]
+      declarations: [ HelloComponent ],
+      imports: [
+        BrowserModule,
+        FormsModule
+      ]
     })
     .compileComponents();
   }));
